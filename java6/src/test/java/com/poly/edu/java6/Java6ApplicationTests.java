5ae035b568402734953d5a54b7f5c3ba3f94d1de
quangtin <quangtin2k5@gmail.gmailcom>
2025-03-18 12:22:36 +0700

    Initial commit

---
+++ java6/src/test/java/com/poly/edu/java6/Java6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.edu.java6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
