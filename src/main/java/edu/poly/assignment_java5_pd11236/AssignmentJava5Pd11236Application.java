649e034384e0e33839d15c5c1f81cf43b94419fc
quangtin <quangtin2k5@gmail.com>
2025-03-31 10:10:01 +0700

    update project

---
+++ src/main/java/edu/poly/assignment_java5_pd11236/AssignmentJava5Pd11236Application.java
@@ -0,0 +1,13 @@
+package edu.poly.assignment_java5_pd11236;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJava5Pd11236Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentJava5Pd11236Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/poly/assignment_java5_pd11236/AssignmentJava5Pd11236ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.poly.assignment_java5_pd11236;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJava5Pd11236ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
